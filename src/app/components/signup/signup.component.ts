ec20e6422b379c21054930d859c35230e312542d
Jordi Piella <jordipiella@gmail.com>
2017-10-28 15:46:52 +0200

    Light corrections

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -31,7 +31,7 @@ export class SignupComponent implements OnInit {
       this.session.signup(this.user)
       .subscribe(
       (data) => {
-        this.router.navigate(['/profile']);
+        this.router.navigate(['/dashboard']);
       },
       (err) => {
         this.error = err
