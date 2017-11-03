4b2298713fd07be855038ec4a50e353909940a40
polgasull <pol89.gn@gmail.com>
2017-11-03 20:12:07 +0100

    dist created and ng build works

--- server.js
+++ server.js
@@ -10,4 +10,8 @@ app.listen(process.env.PORT || 8080);
 
 // PathLocationStrategy
 
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+})
+
 console.log('Console listening!');
