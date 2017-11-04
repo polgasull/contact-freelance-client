7e1054c0490d6dd889e14d80e33d0b48805513e0
polgasull <pol89.gn@gmail.com>
2017-11-04 10:36:39 +0100

    dist fixed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/src/app/dist'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
@@ -11,7 +11,7 @@ app.listen(process.env.PORT || 8080);
 // PathLocationStrategy
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + './src/app/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/src/app/dist/index.html'));
 })
 
 console.log('Console listening!');
