b32b8dab30298ff66777c987345c3aa315951543
polgasull <pol89.gn@gmail.com>
2017-11-04 20:24:09 +0100

    prod shown

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseURL: "", 
-  clientBaseURL: "",
-  googleApiKey: ""
+  baseURL: 'https://contactfreelanceapi.herokuapp.com',
+  clientBaseURL: 'https://contactfreelanceclient.herokuapp.com',
+  googleApiKey: 'AIzaSyBK-_f7kp8ZGLCdeinr8SLXPVQ5fuVvfwY'
 };
