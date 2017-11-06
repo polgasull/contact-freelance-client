7976bb6be7eca63c11ae20361e68a72cfbda8538
polgasull <pol89.gn@gmail.com>
2017-11-06 20:48:51 +0100

    rare characters url

--- src/app/services/helpers.service.ts
+++ src/app/services/helpers.service.ts
@@ -21,7 +21,34 @@ export class HelpersService {
   // función que detecta 2 parametros, los junta, elimina espacio, minusculas y lo convierte en url
   convertToUrl(name, surname, cb) {
     this.str = surname ? name + '-' + surname : name;
-    this.str = encodeURI(this.str.replace(/\s/g, '').replace("'", '').replace("ñ", "n").toLowerCase());
+    this.str = this.str.toLocaleLowerCase();
+    this.str = this.str.replace("à", "a");
+    this.str = this.str.replace("á", "a");
+    this.str = this.str.replace("â", "a");
+    this.str = this.str.replace("ä", "a");
+    this.str = this.str.replace("è", "e");
+    this.str = this.str.replace("é", "e");
+    this.str = this.str.replace("ê", "e");
+    this.str = this.str.replace("ë", "e");
+    this.str = this.str.replace("ì", "i");
+    this.str = this.str.replace("í", "i");
+    this.str = this.str.replace("î", "i");
+    this.str = this.str.replace("ï", "i");
+    this.str = this.str.replace("ò", "o");
+    this.str = this.str.replace("ó", "o");
+    this.str = this.str.replace("ô", "o");
+    this.str = this.str.replace("ö", "o");
+    this.str = this.str.replace("ù", "u");
+    this.str = this.str.replace("ú", "u");
+    this.str = this.str.replace("û", "u");
+    this.str = this.str.replace("ü", "u");
+    this.str = this.str.replace("ç", "c");
+    this.str = this.str.replace(/\s/g, '');
+    this.str = this.str.replace("'", '');
+    this.str = this.str.replace("ñ", "n");
+    this.str = this.str.replace("@", "ae");
+    this.str = this.str.replace("ø", "o")
+    this.str = encodeURI(this.str);
     if (cb) cb(this.str)
     return this.str;
   }
