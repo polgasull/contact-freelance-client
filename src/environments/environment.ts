3039af8bd5c3bbd1e4861431132d23a8e7e97301
Jordi Piella <jordipiella@gmail.com>
2017-11-07 14:31:36 +0100

    env

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    baseURL: 'http://localhost:3000',
+    clientBaseURL: 'http://localhost:4200',
+    googleApiKey: 'AIzaSyBK-_f7kp8ZGLCdeinr8SLXPVQ5fuVvfwY'
+};
